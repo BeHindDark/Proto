587c5dd8bc797f51a031f3609f8516620e9c35e3
jin-sun-web <kanon1359@naver.com>
2020-03-09 18:33:08 +0900

    LoginHttp.cpp LoginHttp.h 추가했습니다. 그리고 Http 작업하도록 하겠습니다.

---
+++ Source/Proto/LoginHttp.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "LoginHttp.h"
+
+// Sets default values
+ALoginHttp::ALoginHttp()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ALoginHttp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ALoginHttp::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
