d22d1110bdcba99614883ca537bdbc049de570c9
jin-sun-web <kanon1359@naver.com>
2020-03-09 17:35:29 +0900

    Pcon_Login.cpp, Pcon_Login.h // 로그인 플레이어 컨트롤러 추가를 했습니다.

---
+++ Source/Proto/Pcon_Login.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Pcon_Login.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTO_API APcon_Login : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
