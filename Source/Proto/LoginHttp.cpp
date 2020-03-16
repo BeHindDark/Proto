8ce504378771bda46b5d8940b8c091a235eec615
jin-sun-web <kanon1359@naver.com>
2020-03-16 10:43:48 +0900

    HTTP.uasset 수정완료

--- Source/Proto/LoginHttp.cpp
+++ Source/Proto/LoginHttp.cpp
@@ -8,27 +8,28 @@
 // Sets default values
 ALoginHttp::ALoginHttp()
 {
- 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	Http = &FHttpModule::Get();
-
 }
 
 
-void ALoginHttp::SendAccountCreationRequest(const FString& userId, const FString& userPw)
+void ALoginHttp::SendAccountCreationRequest(const FString& userId, const FString& userPw, const FString& userName)
 {
-/*
-	동작 순서
-	1. HttpRequest(요청) 생성한다
-	2. 요청 방식은 "Get" or "Post" 방식으로 설정한다
-	3. 요청 URL은 인자값으로 받은 URL을 사용하도록 설정한다
-	4. *중요, 요청 후 프레임마다 받은 바이트의 크기를 리턴해준다	
-	해당 OnAccountCreationResponse 함수를 연결시켜준다
-	5. 요청이 완료됬을 때 호출할 함수를 연결해준다
-	6. 요청을 보내준다
-*/
+	/*
+		동작 순서
+		1. HttpRequest(요청) 생성한다
+		2. 요청 방식은 "Get" or "Post" 방식으로 설정한다
+		3. 요청 URL은 인자값으로 받은 URL을 사용하도록 설정한다
+		4. *중요, 요청 후 프레임마다 받은 바이트의 크기를 리턴해준다
+		해당 OnAccountCreationResponse 함수를 연결시켜준다
+		5. 요청이 완료됬을 때 호출할 함수를 연결해준다
+		6. 요청을 보내준다
+	*/
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("userName : %s, Id : %s, Pw : %s"), *userName, *userId, *userPw));
+
 	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
 	Request->SetVerb("Get");
-	Request->SetURL(FString::Printf(TEXT("http://192.168.116.135::80/d.php?userId=%s&userPw=%s"), *userId, *userPw));
+	Request->SetURL(FString::Printf(TEXT("http://192.168.161.142:8080/join.php?userId=%s&userPw=%s&userName=%s"), *userId, *userPw, *userName));
 	Request->OnProcessRequestComplete().BindUObject(this, &ALoginHttp::OnAccountCreationResponse);
 	Request->SetHeader(TEXT("User-Agent"), "x-UnrealEngin-Agent");
 	Request->SetHeader("Content-Type", TEXT("application/json"));
@@ -37,24 +38,51 @@ void ALoginHttp::SendAccountCreationRequest(const FString& userId, const FString
 
 void ALoginHttp::OnAccountCreationResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
 {
+
+	//웹서버로부터 응답된 내용을 화면에 디버그 문자열로 출력해본다
+	GEngine->AddOnScreenDebugMessage(1, 10.0f, FColor::Green, Response->GetContentAsString());
+
+	/*
 	TSharedPtr<FJsonObject> JsonObject;
 
-	TSharedPtr<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
-	/*
+	//Create a reader pointer to read the json data
+	TSharedRef<TJsonReader<>> Reader = TJsonReaderFactory<>::Create(Response->GetContentAsString());
+
 	if (FJsonSerializer::Deserialize(Reader, JsonObject))
 	{
 		bool bAccountCreatedSuccessfully = JsonObject->GetBoolField("AccountSuccessfullyCreated");
-		FString Username = JsonObject->GetStringField("Username");
+		FString userId = JsonObject->GetStringField("userId");
+
+		//GEngine->AddOnScreenDebugMessage(1, 30.0f, FColor::Blue, FString::Printf(TEXT("%s"), userId));
 	}
 	*/
-	
+}
+
+void ALoginHttp::SendLoginRequest(const FString& userId, const FString& userPw)
+{
+	GEngine->AddOnScreenDebugMessage(-1, 5.0f, FColor::Red, FString::Printf(TEXT("userName : Id : %s, Pw : %s"), *userId, *userPw));
+
+	TSharedRef<IHttpRequest> Request = Http->CreateRequest();
+	Request->SetVerb("Get");
+	Request->SetURL(FString::Printf(TEXT("http://192.168.161.142:8080/login.php?userId=%s&userPw=%s&userName=%s"), *userId, *userPw));
+	Request->OnProcessRequestComplete().BindUObject(this, &ALoginHttp::OnLoginResponse);
+	Request->SetHeader(TEXT("User-Agent"), "x-UnrealEngin-Agent");
+	Request->SetHeader("Content-Type", TEXT("application/json"));
+	Request->ProcessRequest();
+}
+
+void ALoginHttp::OnLoginResponse(FHttpRequestPtr Request, FHttpResponsePtr Response, bool bWasSuccessful)
+{
+	//웹서버로부터 응답된 내용을 화면에 디버그 문자열로 출력해본다
+	GEngine->AddOnScreenDebugMessage(1, 10.0f, FColor::Green, Response->GetContentAsString());
 }
 
 // Called when the game starts or when spawned
 void ALoginHttp::BeginPlay()
 {
 	Super::BeginPlay();
-	
+
+
 }
 
 // Called every frame
