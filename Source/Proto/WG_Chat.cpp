88ef00f7b46c9e0c902a7f66175520aa9db19e20
jin-sun-web <kanon1359@naver.com>
2020-04-02 18:28:15 +0900

    PC_Lobby.cpp 코드 추가 WG_SessionLobby.h 코드 추가

--- Source/Proto/WG_Chat.cpp
+++ Source/Proto/WG_Chat.cpp
@@ -1,11 +1,22 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+    // Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "WG_Chat.h"
+#include "PC_Lobby.h"
+#include "WG_TextBox.h"
+#include "Components/EditableTextBox.h"
+
+UWG_Chat::UWG_Chat(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	static ConstructorHelpers::FClassFinder<UWG_TextBox> WG_TextBox_C(TEXT("/Game/Blueprints/Widget/UMG_TextBox.UMG_TextBox_C"));
+	if (WG_TextBox_C.Succeeded()) {
+		WG_TextBoxClass = WG_TextBox_C.Class;
+	}
+}
 
 void UWG_Chat::NativeConstruct()
 {
-
+	ChatEnty->OnTextCommitted.AddDynamic(this, &UWG_Chat::ChattingCommitted);
 }
 
 void UWG_Chat::ChattingCommitted(const FText& Input, ETextCommit::Type InCommitType)
@@ -14,11 +25,20 @@ void UWG_Chat::ChattingCommitted(const FText& Input, ETextCommit::Type InCommitT
 	{
 		if (Input.IsEmpty())
 		{
-
+			ChatEnty->SetText(Input);
+			//GetOwningPlayer()->SetInputMode(GetOwningPlayer())
 		}
 		else
 		{
-
+			APC_Lobby* PC_Lobby = Cast<APC_Lobby>(GetOwningPlayer());
 		}
+	}
+}
+
+void UWG_Chat::AddChatLog(const FText& UserName, const FText& Message)
+{
+	if (GetOwningPlayer()->IsLocalPlayerController())
+	{
+		
 	}
 }
