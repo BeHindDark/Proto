6d18f0768af61abb7b32acf0a65a557f759f2cae
cjsdnd2001 <cjsdnd2001@gmail.com>
2020-04-03 18:46:56 +0900

    파티클추가 (아직안됨)

--- Source/Proto/Anim_DB_Weapon_AnimInstance.cpp
+++ Source/Proto/Anim_DB_Weapon_AnimInstance.cpp
@@ -5,10 +5,15 @@
 
 void UAnim_DB_Weapon_AnimInstance::AnimNotify_UpFireCheck()
 {
+	/** 위쪽 총열에서 총알이 나갈타이밍에 실행되는 함수입니다. 그타이밍에 브로드캐스트해줍니다.
+	*/
 	UpFireCheck.Broadcast();
+	
 }
 
 void UAnim_DB_Weapon_AnimInstance::AnimNotify_DownFireCheck()
 {
+	/** 아래쪽 총열에서 총알이 나갈타이밍에 실행되는 함수입니다. 그타이밍에 브로드캐스트해줍니다.
+	*/
 	DownFireCheck.Broadcast();
 }
